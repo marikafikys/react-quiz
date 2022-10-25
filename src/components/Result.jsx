59098e46625d954af0e8989498ed89596c1fc26b
marikafikys <marikaafikys@gmail.com>
2022-10-25 19:54:21 +0400

    added refreshPage

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,9 +1,14 @@
 export function Result({ correct, questions }) {
+
+	function refreshPage() {
+		window.location.reload(false);
+	 }
+
 	return (
 	  <div className="result">
 		 <img src="https://cdn-icons-png.flaticon.com/512/5511/5511415.png" alt="congrats"/>
 		 <h2>Количество верных ответов: {correct} из {questions.length}</h2>
-		 <a href="/"><button>Попробовать снова</button></a>
+		 <button onClick={refreshPage}>Попробовать снова</button>
 	  </div>
 	);
  }
